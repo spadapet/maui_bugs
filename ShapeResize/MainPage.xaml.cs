19b3299b0ab28d2062429807147c6713b7e7bcc9
Peter Spada <peterspa@microsoft.com>
2023-10-10 13:41:06 -0700

    Add ShapeResize project

---
+++ ShapeResize/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace ShapeResize
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked_Add(object sender, EventArgs e)
+        {
+            line.Y2 = Math.Min(400.0, line.Y2 + 50.0);
+        }
+
+        private void Button_Clicked_Subtract(object sender, EventArgs e)
+        {
+            line.Y2 = Math.Max(0.0, line.Y2 - 50.0);
+        }
+    }
+}
