b27e16db0d26a4b07131ad90e41b5015cf1bf7e0
Peter Spada <peterspa@microsoft.com>
2024-02-06 15:40:23 -0800

    Add GridRowChanges project

---
+++ GridRowChanges/GridRowChanges/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+
+namespace GridRowChanges
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private int gridRow = 1;
+        public int GridRow
+        {
+            get => gridRow;
+            set
+            {
+                if (gridRow != value)
+                {
+                    gridRow = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(GridRow)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        public ViewModel ViewModel { get; } = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ViewModel.GridRow = ViewModel.GridRow == 0 ? 1 : 0;
+        }
+    }
+}
