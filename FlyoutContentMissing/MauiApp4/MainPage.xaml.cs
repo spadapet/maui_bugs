1cf90010c01dd75d95114a4a459927b6a5a13536
Peter Spada <peterspa@microsoft.com>
2024-05-09 21:56:17 -0700

    Add FlyoutContentMissing

---
+++ FlyoutContentMissing/MauiApp4/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiApp4
+{
+    public partial class MainPage : ContentPage
+    {
+        private object? oldFlyoutContent;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            if (Application.Current?.MainPage is Shell shell)
+            {
+                object oldFlyoutContent = shell.FlyoutContent;
+                shell.FlyoutContent = this.oldFlyoutContent;
+                this.oldFlyoutContent = oldFlyoutContent;
+            }
+        }
+    }
+
+}
