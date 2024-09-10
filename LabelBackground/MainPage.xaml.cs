b72f52211903cc3065d7f615c71aea15e0cf3645
Peter Spada <peterspa@microsoft.com>
2024-09-10 15:09:50 -0700

    Add LabelBackground project

---
+++ LabelBackground/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace LabelBackground;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnSetBackgroundClicked(object sender, EventArgs e)
+	{
+		this.label.Background = new SolidColorBrush(Colors.Yellow);
+	}
+
+    private void OnClearBackgroundClicked(object sender, EventArgs e)
+    {
+		this.label.Background = null;
+    }
+}
+
